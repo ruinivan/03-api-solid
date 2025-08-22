3e03388e2a44d122e2361f4dcb022d4f1e2c293d
Victor <victorruinivanvg@gmail.com>
2025-08-22 13:12:19 -0300

    feat: :zap: avanaçando aos poucos

---
+++ Api-Solid_PostgreSQL.bat
@@ -0,0 +1,1 @@
+docker run --name api-solid-pg -e POSTGRESQL_USERNAME=docker -e POSTGRESQL_PASSWORD=docker -e POSTGRESQL_DATABASE=apisolid -p 5432:5432 bitnami/postgresql
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Victor',
+    email: 'victorruinivanvg@gmail.com',
+  },
+})
 
 // ORM - Object Relational Mapper
