a4ebf9e43166b93e9557a96eec0fed49a75dee5a
Victor <victorruinivanvg@gmail.com>
2025-08-28 17:25:00 -0300

    feat: :art: Starting a new test

--- src/http/services/check-in.spec.ts
+++ src/http/services/check-in.spec.ts
@@ -13,10 +13,24 @@ describe('Register Service', () => {
 
   test('It should be able to check in', async () => {
     const { checkIn } = await sut.execute({
-      userId: '',
-      gymId: '',
+      userId: 'user-01',
+      gymId: 'gym-01',
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  test('It should not be able to check in twich in the same day', async () => {
+    await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        userId: 'user-01',
+        gymId: 'gym-01',
+      }),
+    ).toEqual(expect.any(String))
   })
 })
