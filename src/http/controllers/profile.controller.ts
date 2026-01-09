9697b24e980a8ef003369a88ec4156601bdc98fa
Victor Silvera <victorruinivanvg@gmail.com>
2026-01-09 17:05:17 -0300

    feat: :art: Criando os controllers

---
+++ src/http/controllers/profile.controller.ts
@@ -0,0 +1,25 @@
+import type { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function profileController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  // const authenticateBodySchema = z.object({
+  //   email: z.string().email(),
+  //   password: z.string().min(6),
+  // })
+  // const { email, password } = authenticateBodySchema.parse(request.body)
+  // try {
+  //   const authenticateService = authenticateServiceFactory()
+  //   await authenticateService.execute({
+  //     email,
+  //     password,
+  //   })
+  // } catch (error) {
+  //   if (error instanceof InvalidCredentialsError) {
+  //     return reply.status(400).send({ message: error.message })
+  //   }
+  //   throw error
+  // }
+  return reply.status(200).send()
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from 'fastify'
 import { registerController } from './controllers/register.controller'
 import { authenticateController } from './controllers/authenticate.controller'
+import { profileController } from './controllers/profile.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
   app.post('/sessions', authenticateController)
+
+  /** Authenticaded */
+  app.get('/me', profileController)
 }
